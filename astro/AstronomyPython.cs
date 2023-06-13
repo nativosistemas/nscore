cbf6a54e2a47f189c5dcd134d3e2bfd1afd175a0
nativosistemas <nativosistemas@outlook.com.ar>
2023-06-12 23:20:16 -0300

    ajax6

--- astro/AstronomyPython.cs
+++ astro/AstronomyPython.cs
@@ -46,19 +46,31 @@ public class AstronomyPython : IDisposable
         string result = string.Empty;
         try
         {
+            string envPythonHome = @"C:\Python39\python39.dll";
+            ///usr/bin/python2.7
+            if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Linux))
+            {
+                envPythonHome = @"/usr/bin/python2.7";
+            }
+            // your PATH environment variable as well.
+            Runtime.PythonDLL = envPythonHome;//"python310.dll";
+
+            PythonEngine.Initialize();
+
             using (Py.GIL()) // Inicializa el Global Interpreter Lock (GIL) de Python
             {
-                      var nameFile = Path.Combine(nscore.Util.WebRootPath, @"files", "py_servo.py");
-            //if (File.Exists(nameFile))
-           // {}
-                dynamic py = Py.Import("__main__"); // Importa el módulo principal de Python
-                py.exec(File.ReadAllText(nameFile)); // Ejecuta el script Python "py_servo.py"
+                var nameFile = Path.Combine(nscore.Util.WebRootPath, @"files", "py_servo.py");
+                if (File.Exists(nameFile))
+                {
+                    dynamic py = Py.Import("__main__"); // Importa el módulo principal de Python
+                    py.exec(File.ReadAllText(nameFile)); // Ejecuta el script Python "py_servo.py"
 
-                dynamic moveServo = py.moveServo; // Obtiene la referencia a la función Python
+                    dynamic moveServo = py.moveServo; // Obtiene la referencia a la función Python
 
-                moveServo = moveServo(Convert.ToInt32(pServoCoordinates.servoH),Convert.ToInt32(pServoCoordinates.servoV),1); // Llama a la función Python
+                    moveServo = moveServo(Convert.ToInt32(pServoCoordinates.servoH), Convert.ToInt32(pServoCoordinates.servoV), 1); // Llama a la función Python
 
-                Console.WriteLine(result); // Imprime el resultado
+                    Console.WriteLine(result); // Imprime el resultado
+                }
             }
 
             // string responseData = Convert.ToInt32(pServoCoordinates.servoH).ToString() + "_" + Convert.ToInt32(pServoCoordinates.servoV).ToString() + "_0";// "Hola desde el servidor";
