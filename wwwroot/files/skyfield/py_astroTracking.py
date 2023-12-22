72799fff80a6ea53deab3cf4909d31fe791348c6
nativosistemas <nativosistemas@outlook.com.ar>
2023-12-22 16:39:57 -0300

    astroTracking

--- wwwroot/files/skyfield/py_astroTracking.py
+++ wwwroot/files/skyfield/py_astroTracking.py
@@ -5,6 +5,8 @@ import sys
 import time
 import json
 import platform
+from py_util import getConfig
+import subprocess
 
 def decimal_a_tiempo(valor):
     decimal = (valor * 24.0) / 360.0
@@ -50,37 +52,49 @@ while True:
     # Consultar todos los registros de la tabla usuarios
     cursor.execute('SELECT * FROM AstroTrackings WHERE estado = 1')
     registros = cursor.fetchall()
-    city = earth + wgs84.latlon(-32.94681944444444 ,  -60.6393194444444 )
+    oConfig = getConfig()
+    city = earth + wgs84.latlon(oConfig.latitude , oConfig.longitude )
     #print("Registros actuales en la tabla:")
     for registro in registros:
-        #print( registro[3]) # ra
-        #print( registro[4]) # dec
         ra = registro[3]
         dec = registro[4]
         
-
         t = ts.now()
 
         barnard2 = Star(ra_hours=decimal_a_tiempo(ra), dec_degrees=decimal_a_grado(dec))
 
-        astrometric_star = rosario.at(t).observe(barnard2)  
+        astrometric_star = city.at(t).observe(barnard2)  
 
         local = astrometric_star.apparent()
 
         altitud, azimut, d = local.altaz()
         # Actualizar 
         publicID = registro[1]
-        #print(publicID)
-        #Altitude = 0
-        #Azimuth = 1
+
         cursor.execute('UPDATE AstroTrackings SET Altitude=?,Azimuth=?,estado=? WHERE publicID=?', (altitud.degrees,azimut.degrees,2, publicID))
-        # fin Actualizar
+        #
+        horizontal = azimut
+        vertical = altitud
+
+        if azimut < 180.0:
+            horizontal = 180.0 - azimut
+            vertical = 180.0 - altitud
+        else:
+            horizontal = 360.0 - azimut    
+
+        parametroH = horizontal#float(sys.argv[1])
+        parametroV = vertical#float(sys.argv[2])
+        parametroLaser = 0
+        parametroH_rango_min = oConfig.horizontal_grados_min
+        parametroH_rango_max = oConfig.horizontal_grados_max
+        parametroV_rango_min = oConfig.vertical_grados_min
+        parametroV_rango_max = oConfig.vertical_grados_max
+        sleep_secs = 3#float(sys.argv[8])       
+        subprocess.call(['python', 'py_astro_servos.py', parametroH, parametroV,parametroLaser,parametroH_rango_min,parametroH_rango_max,parametroV_rango_min,parametroV_rango_max,sleep_secs])
+        #
         print(registro)
 
-
-    # Guardar (commit) los cambios
     conexion.commit()
-    # Pausa de 1 segundo entre las consultas
     time.sleep(0.5)
     
 # Cerrar el cursor y la conexion
