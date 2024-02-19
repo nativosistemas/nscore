058843362aa1f90adcb5d0bce373111fbee4dc9c
nativosistemas <nativosistemas@outlook.com.ar>
2024-02-19 18:40:17 -0300

    servo3

--- io/ProcessAntV2.cs
+++ io/ProcessAntV2.cs
@@ -159,7 +159,7 @@ public class ProcessAntV2 : IDisposable
         HorizontalCoordinates resault = null;
         int contador = 0;
 
-        while (contador < 10)
+        while (contador < 50)
         {
             using (var context = new AstroDbContext())
             {
@@ -177,7 +177,7 @@ public class ProcessAntV2 : IDisposable
                     break;
                 }
             }
-            await Task.Delay(200);
+            await Task.Delay(250);
             contador++;
         }
         return resault;
