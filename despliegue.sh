f2485b7250ec019a208aac5d02dff1955a17e2eb
nativosistemas <nativosistemas@outlook.com.ar>
2024-03-29 17:00:37 -0300

    script

--- despliegue.sh
+++ despliegue.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-systemctl stop nscore.service
+sudo systemctl stop nscore.service
 
 # Detener y eliminar el contenedor existente (si existe)
-docker stop nscore && docker rm nscore
+sudo docker stop nscore && sudo docker rm nscore
 
 # Eliminar la imagen existente (si existe)
 sudo docker rmi nscore
 
 # Construir la nueva imagen de Docker
-docker-compose build
+sudo docker-compose build
 
 # Desplegar la nueva imagen de Docker
-docker-compose up -d
+sudo docker-compose up -d
 
-systemctl start nscore.service
+sudo systemctl start nscore.service
