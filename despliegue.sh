386a8ebd920bba4b1908d1d5e3c1d78669524267
nativosistemas <nativosistemas@outlook.com.ar>
2024-03-29 17:09:14 -0300

    script2

--- despliegue.sh
+++ despliegue.sh
@@ -9,9 +9,14 @@ sudo docker stop nscore && sudo docker rm nscore
 sudo docker rmi nscore
 
 # Construir la nueva imagen de Docker
-sudo docker-compose build
+#sudo docker-compose build
 
 # Desplegar la nueva imagen de Docker
-sudo docker-compose up -d
+#sudo docker-compose up -d
+
+# INICIO TEMPORAL (FALTA docker-compose)
+sudo docker build -t nscore .
+sudo docker run -p 80:8080  -v /usr/src/dockerns:/usr/src  --name nscore --hostname nscore  -d nscore
+# FIN TEMPORAL (FALTA docker-compose)
 
 sudo systemctl start nscore.service
