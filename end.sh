ea682874b596a7e044495832908c72030f17960e
nativosistemas <nativosistemas@outlook.com.ar>
2024-09-15 23:52:15 -0300

    sh3

--- end.sh
+++ end.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 sudo systemctl stop nscore-python.service
-sudo docker stop nscore-python && sudo docker rm nscore-python
-sudo docker rmi nscore-python
+sudo docker stop nscore-python 
 
 sudo systemctl stop nscore.service
-sudo docker stop nscore && sudo docker rm nscore
-sudo docker rmi nscore
+sudo docker stop nscore 
