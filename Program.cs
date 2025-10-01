871a5aa5a964081e9b3877c170ea0c1382ff2c9a
nativosistemas <nativosistemas@outlook.com.ar>
2025-10-01 00:12:56 -0300

    auth

--- Program.cs
+++ Program.cs
@@ -148,7 +148,7 @@ internal class Program
      return Results.Unauthorized();
  });
         app.MapGet("/estrellas", async (System.Security.Claims.ClaimsPrincipal user, nscore.ProcessAntV2 pProcessAntV2) => { return Results.Json(pProcessAntV2.getStars()); }).RequireAuthorization();
-        app.MapPost("/actionAnt", async (nscore.ProcessAntV2 pProcessAntV2, [Microsoft.AspNetCore.Mvc.FromBody]ActionAntRequest pValue) => { return await pProcessAntV2.actionAnt(pValue); });
+        app.MapPost("/actionAnt", async (nscore.ProcessAntV2 pProcessAntV2, [Microsoft.AspNetCore.Mvc.FromBody]ActionAntRequest pValue) => { return await pProcessAntV2.actionAnt(pValue); }).RequireAuthorization();
 
         app.Run();
 
