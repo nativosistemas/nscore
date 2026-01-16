a57b682c7fc5caa6005b2db5fd0926410c4160f5
nativosistemas <nativosistemas@outlook.com.ar>
2026-01-16 01:03:18 -0300

    web7

--- nscore.sh
+++ nscore.sh
@@ -6,7 +6,7 @@ echo "=== Deteniendo y eliminando contenedores ==="
 docker-compose down
 
 echo -e "\n=== Levantando contenedores en segundo plano ==="
-docker-compose up -d
+docker-compose up -d --pull always
 
 echo -e "\n=== Estado de los contenedores ==="
 docker-compose ps
